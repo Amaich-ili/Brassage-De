2a7ba393a95635392223318f917640a2d7350159
Iliass <iliamaich@gmail.com>
2022-08-23 17:36:29 -0400

    Création de la classe dés

---
+++ Iliass_Amaich_labo1.py
@@ -0,0 +1,11 @@
+# Iliass Amaich
+from random import randint
+
+
+class Dés:
+    def __init__(self, nb_face = 24, dé_1 = 0, dé_2 = 0):
+        self.nb_face = nb_face
+        self.dé_1 = dé_1
+        self.dé_2 = dé_2
+
+  
